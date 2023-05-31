08782eea51cc18d1c70c634183165f4f1ca7e25f
Davy Paulino <37334813+ThreeDP@users.noreply.github.com>
2023-05-31 23:37:23 +0000

    feat(msgs & struct init) Add the messages of philos and the init data struct

--- src/philo/philo.c
+++ src/philo/philo.c
@@ -6,13 +6,45 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 21:19:41 by dapaulin          #+#    #+#             */
-/*   Updated: 2023/05/31 21:20:45 by dapaulin         ###   ########.fr       */
+/*   Updated: 2023/05/31 23:24:01 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(int ac, char **av)
+t_setting	*start_setting(int ac, char **av)
 {
+    t_setting	*s;
 
+    s = malloc(sizeof(t_setting));
+    memset(s, 0, sizeof(t_setting));
+    s->num_philos = atoi(av[1]);
+    s->tm_die = atoi(av[2]);
+    s->tm_eat = atoi(av[3]);
+    s->tm_sleep = atoi(av[4]);
+    if (ac == 6)
+	   s->num_tm_eat = atoi(av[5]);
+    return (s);
+}
+
+void	clean_all(t_setting *s)
+{
+	if (s)
+		free(s);
+}
+
+int	main(int ac, char **av)
+{
+	t_setting	*s;
+
+	s = NULL;
+	if (ac >= 5 && ac <= 6)
+	{
+		s = start_setting(ac, av);
+		clean_all(s);
+		return (0);
+	}
+	printf("extra number of arguments\n");
+	return (0);
+	(void)s;
 }
--- src/philo/philo.h
+++ src/philo/philo.h
@@ -7,4 +7,19 @@
 # include <unistd.h>
 # include <pthread.h>
 
+typedef struct s_setting
+{
+    int     num_philos;
+    int     tm_die;
+    int     tm_eat;
+    int     tm_sleep;
+    int     num_tm_eat;
+}           t_setting;
+
+# define DIE_MSG "%i %i died\n"
+# define EAT_MSG "%i %i is eating\n"
+# define THINK_MSG "%i %i is thinking\n"
+# define SLEEP_MSG "%i %i is sleeping\n"
+# define FORK_MSG "%i %i has taken a fork\n"
+
 #endif
